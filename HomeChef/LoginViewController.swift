0914be4223456533f51e8f9c82aace042b2a755d
Mantaqaa Khan <parshakhan@icloud.com>
2019-04-12 18:18:45 -0400

    background image and login view

---
+++ HomeChef/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  HomeChef
+//
+//  Created by Mantaqaa Khan on 4/12/19.
+//  Copyright © 2019 Joyce Huang. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        assignBackground()
+        // Do any additional setup after loading the view.
+    }
+    func assignBackground() {
+        let background = UIImage(named: "Background")
+    }
+
+        // Do any additional setup after loading the view.
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
